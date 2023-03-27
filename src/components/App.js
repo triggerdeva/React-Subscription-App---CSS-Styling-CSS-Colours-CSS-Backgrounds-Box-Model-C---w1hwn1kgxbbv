078be20b622f1b79b65d6e7deb96d3dbdc977a4b
DEVENDRA MESHRAM <120394539+triggerdeva@users.noreply.github.com>
2023-03-27 19:32:17 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,19 +3,49 @@ import "../styles/App.css";
 const App = () => {
   return (
     <div id="main">
-      <div class="container">
-                <div class="community">
-                  <h2 class="community-header">Join our community</h2>
-                </div>
-                <div class="card-details">
-                  <div class="card">
-                    <p class="card-header">Monthly Subscription</p>
-                  </div>
-                  <div class="why-us">
-                    <h4 class="why-us-header">Why Us</h4>
-                  </div>
-                </div>
+      <section className="card-section">
+        <div className="container">
+          <div className="community">
+            <h2 className="community-header">Join our community</h2>
+            <h3 className="community-sub-header">
+              30-day, hassle-free monkey back gurarantee
+            </h3>
+            <p className="community-text">
+              Gain access to our full library of tutorials along with expert
+              code reviews. Perfect for any developers who are serious about
+              honing their skills.
+            </p>
+          </div>
+          <div className="card-details">
+            <div className="card">
+              <p className="card-header">Monthly Subscription</p>
+              <div className="price">
+                <span>$29</span>
+                <p>Per Month</p>
               </div>
+              <p className="card-details-text">
+                Full access for less than $1 a day.
+              </p>
+              <a href="#" className="btn btn-card">
+                Sign Up
+              </a>
+            </div>
+            <div className="why-us">
+              <h4 className="why-us-header">Why Us</h4>
+              <div className="why-us-lists">
+                <ul className="why-us-items">
+                  <li className="why-us-item">Tutorials by industry experts</li>
+                  <li className="why-us-item">Peer & expert code review</li>
+                  <li className="why-us-item">Coding exercises</li>
+                  <li className="why-us-item">Access yo our GitHub repos</li>
+                  <li className="why-us-item">Community forum</li>
+                  <li className="why-us-item">Flashcard decks</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
